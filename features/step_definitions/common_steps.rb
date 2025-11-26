eb79c8f845aab91937c3662201e5708a2b66798a
Andressa Karla <andressa.karla1@gmail.com>
2025-11-25 23:25:44 -0300

    fix: Adicionando uma outra estratégia de espera de um step do cenário pesquisar produto cadastrado para verificar se será ou não executado com sucesso no gh actions

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -26,8 +26,7 @@ Dado('que na seção Cadastrar Usuários eu clique no botão Cadastrar') do
 end
 
 Dado('que eu esteja na tela Cadastro de usuários') do
-    sleep 2
-    expect(home.current_url).to include('/admin/cadastrarusuarios')
+    expect(page).to have_current_path(/\/admin\/cadastrarusuarios/, wait: 10)
 end
 
 Dado('que eu informe os campos de {string}, {string} e {string} e eu NÃO informe a opcão Cadastrar como administrador') do |nome, email, senha|
